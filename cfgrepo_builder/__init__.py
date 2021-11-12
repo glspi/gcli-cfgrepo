d49bbff2ce49f400ebf8e35ee4e25c866f8c3147
Ryan Gillespie <rgillespie@gmail.com>
2021-11-12 16:42:02 -0700

    initial

---
+++ cfgrepo_builder/__init__.py
@@ -0,0 +1,1 @@
+"cfgrepo_builder"
---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+__version__ = "0.0.1"
+
+with open("requirements.txt", "r", encoding="utf-8") as f:
+    INSTALL_REQUIRES = f.read().splitlines()
+
+setuptools.setup(
+    name="gcli-cfgrepo",
+    version=__version__,
+    packages=["cfgrepo_builder"],
+    install_requires=INSTALL_REQUIRES,
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX :: Linux",
+        "Operating System :: MacOS",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3 :: Only",
+    ],
+    python_requires=">=3.7",
+    entry_points={"console_scripts": ["cfgrepo = cfgrepo_builder.cfgrepo_builder:main"]},
+)
